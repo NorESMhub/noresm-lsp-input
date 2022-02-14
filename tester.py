465c28e49a388c93f20c58564b6f5e62581533b6
lasseke <57358428+lasseke@users.noreply.github.com>
2022-02-14 13:27:48 +0100

    Delete tester.py

--- tester.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-with open(r"C:\Git_repos\nlp-input-handling\site_input_instructions.yaml", 'r') as stream:
-    dictionary = yaml.safe_load(stream)
-for key, value in dictionary.items():
-    print (key + " : " + str(value))
