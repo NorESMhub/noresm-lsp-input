e43c04e2d2574ed0d832fbd2508fa9c8cfa60289
lasseke <57358428+lasseke@users.noreply.github.com>
2022-07-05 15:37:55 +0200

    Add LSP input instruction yamls and corresponding SAGA sbatch scripts

---
+++ make_input_jobscripts/v1.0.0/create_input_ALP4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=nn2806k
+#SBATCH --time=10:00:00
+## You can add more args for sbatch here in the same way as above (see "sbatch --help" for all the args), e.g.:
+#SBATCH --qos=normal
+#SBATCH --job-name=ALP4_input
+
+## Your commands here, e.g.:
+module --quiet purge  # Reset the modules to the system default
+cd ~/nlp-input-handling
+. ./load_dependencies.sh
+
+python3 create_forcing_classic.py -f ./lsp_site_instructions/v1.0.0/site_input_ALP4.yaml
