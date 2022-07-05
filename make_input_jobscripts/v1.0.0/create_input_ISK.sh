e163549e663fdcabc50ec0b94b1d62e54f8f724f
lasseke <57358428+lasseke@users.noreply.github.com>
2022-07-05 20:39:38 +0200

    Add LATICE site job scripts for SAGA

---
+++ make_input_jobscripts/v1.0.0/create_input_ISK.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=nn2806k
+#SBATCH --time=10:00:00
+## You can add more args for sbatch here in the same way as above (see "sbatch --help" for all the args), e.g.:
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=4
+#SBATCH --job-name=ISK_input
+
+## Your commands here, e.g.:
+module --quiet purge  # Reset the modules to the system default
+cd ~/nlp-input-handling
+. ./load_dependencies.sh
+
+python3 create_forcing_classic.py -f ./lsp_site_instructions/v1.0.0/site_input_ISK.yaml
