9bfdf1ea1d3e567a4261ace2d9f27afcdefe5927
lasseke <57358428+lasseke@users.noreply.github.com>
2022-07-11 12:14:04 +0200

    Remove logging, minor cosmetic changes

--- create_forcing_classic.py
+++ create_forcing_classic.py
@@ -28,24 +28,23 @@ There are two ways to execute this script:
 
 Parts of the code are heavily inspired by NCARs subset_data.py tool:
 https://github.com/ESCOMP/CTSM/blob/master/tools/site_and_regional/subset_data.py
+and other resources provided by NCAR and the CESM hive mind.
+
+Authors: Lasse Keetz, Hui Tang
+Date: 2022-07-11
 """
 
 # Library imports
-# Python standard library
-import sys
-import logging
-import logging.handlers
-import subprocess
 import glob
+import subprocess
+import sys
 import yaml
-import string
-import random
 
+from argparse import ArgumentParser, RawTextHelpFormatter
 from datetime import date
-from argparse import ArgumentParser
-from argparse import RawTextHelpFormatter
 from pathlib import Path
 from typing import Union
+
 
 ###############################################################################
 """
@@ -76,96 +75,6 @@ def get_parser():
                         + "'machine_properties.yaml'", action="store",
                         dest="machine", required=False, default='saga')
     return parser
-
-
-###############################################################################
-"""
-Define logger
-"""
-###############################################################################
-
-
-class StreamToLogger(object):
-    """
-    Custom class to log all stdout and stderr streams.
-    modified from:
-    https://www.electricmonk.nl/log/2011/08/14/redirect-stdout-and-stderr-to-a-logger-in-python/
-    """
-
-    def __init__(self, stream, logger, log_level=logging.INFO,
-                 also_log_to_stream=False):
-        self.logger = logger
-        self.stream = stream
-        self.log_level = log_level
-        self.linebuf = ''
-        self.also_log_to_stream = also_log_to_stream
-
-    @classmethod
-    def setup_stdout(cls, also_log_to_stream=True):
-        """
-        Setup logger for stdout
-        """
-        stdout_logger = logging.getLogger('STDOUT')
-        sl = StreamToLogger(sys.stdout, stdout_logger, logging.INFO,
-                            also_log_to_stream)
-        sys.stdout = sl
-
-    @classmethod
-    def setup_stderr(cls, also_log_to_stream=True):
-        """
-        Setup logger for stdout
-        """
-        stderr_logger = logging.getLogger('STDERR')
-        sl = StreamToLogger(sys.stderr, stderr_logger, logging.ERROR,
-                            also_log_to_stream)
-        sys.stderr = sl
-
-    def write(self, buf):
-        temp_linebuf = self.linebuf + buf
-        self.linebuf = ''
-        for line in temp_linebuf.splitlines(True):
-            if line[-1] == '\n':
-                self.logger.log(self.log_level, line.rstrip())
-            else:
-                self.linebuf += line
-
-    def flush(self):
-        if self.linebuf != '':
-            self.logger.log(self.log_level, self.linebuf.rstrip())
-        self.linebuf = ''
-
-###############################################################################
-
-
-def setup_logging(log_file, log_level):
-    """
-    Setup logging to log to console and log file.
-    """
-
-    logging.raiseExceptions = True
-    root_logger = logging.getLogger()
-    root_logger.setLevel(log_level)
-
-    # setup log file
-    one_mb = 1000000
-    handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=one_mb,
-                                                   backupCount=10)
-
-    fmt = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-        datefmt='%y-%m-%d %H:%M:%S')
-
-    handler.setFormatter(fmt)
-    root_logger.addHandler(handler)
-
-    # setup logging to console
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setFormatter(fmt)
-    root_logger.addHandler(stream_handler)
-
-    # redirect stdout/err to log file
-    StreamToLogger.setup_stdout()
-    StreamToLogger.setup_stderr()
 
 
 ###############################################################################
@@ -229,7 +138,9 @@ class Machine:
 
 
 class SinglePointExtractor:
-    """Documentation!"""
+    """
+    Utility class to create single-point CTSM forcing from global NetCDF files.
+    """
 
     minimum_required = ('surface', 'urban', 'aging_parameters',
                         'optical_properties', 'fire', 'clm', 'fates',
@@ -684,7 +595,8 @@ class SinglePointExtractor:
         nc_in_str = \
             self.instruction_dict['nc_input_paths']['land']['urban']
 
-        out_path = self.output_dir / 'lnd' / 'clm2' / 'urbandata' / str(self.site_code)
+        out_path = self.output_dir / 'lnd' / \
+            'clm2' / 'urbandata' / str(self.site_code)
         if not out_path.is_dir():
             self.make_dir(out_path)
         out_str = str(out_path)
@@ -758,7 +670,8 @@ class SinglePointExtractor:
         nc_in_str = \
             self.instruction_dict['nc_input_paths']['land']['fire']
 
-        out_path = self.output_dir / 'lnd' / 'clm2' / 'firedata' / str(self.site_code)
+        out_path = self.output_dir / 'lnd' / \
+            'clm2' / 'firedata' / str(self.site_code)
         if not out_path.is_dir():
             self.make_dir(out_path)
         out_str = str(out_path)
@@ -898,7 +811,8 @@ class SinglePointExtractor:
         nc_in_str = \
             self.instruction_dict['nc_input_paths']['atmosphere']['lightning']
 
-        out_path = self.output_dir / 'atm' / 'datm7' / 'NASA_LIS' / str(self.site_code)
+        out_path = self.output_dir / 'atm' / \
+            'datm7' / 'NASA_LIS' / str(self.site_code)
         if not out_path.is_dir():
             self.make_dir(out_path)
         out_str = str(out_path)
@@ -974,7 +888,6 @@ class SinglePointExtractor:
 
 ###############################################################################
 
-
     def tar_output(self):
         """Compress the files in the specified output dir into a Tarball"""
 
@@ -1017,22 +930,8 @@ Main function
 
 def main():
 
+    # Parse command line arguments
     args = get_parser().parse_args()
-
-    """
-    # DISABLE LOGGING FOR TESTING, SOMETIMES CREATES STACK OVERFLOW ERRORS
-    today = date.today()
-    today_string = today.strftime("%Y-%m-%d")
-
-    work_dir = Path(__file__).parent
-    # Generate log file name, random ending to avoid access errors
-    rand_str = ''.join(random.choices(string.ascii_uppercase
-                                      + string.digits, k=6))
-    log_file = work_dir / f'{today_string}-{rand_str}.log'
-    log_level = logging.DEBUG
-    setup_logging(log_file, log_level)
-    log = logging.getLogger(__name__)
-    """
 
     # Instantiate machine class
     machine = Machine(name=args.machine)
